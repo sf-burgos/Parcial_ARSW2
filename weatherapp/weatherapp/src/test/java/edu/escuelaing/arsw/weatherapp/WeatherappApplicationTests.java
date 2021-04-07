f65c50e6e729fbe6c60fea2b5084d8646ce29420
Brayan Burgos <brayan.burgos@mail.escuelaing.edu.co>
2021-04-07 09:08:56 -0500

    fp

---
+++ weatherapp/weatherapp/src/test/java/edu/escuelaing/arsw/weatherapp/WeatherappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
